848dc55f2b36cac9ac1d9af2ead5329502d0df85
陈淇格 <cqg186495@outlook.com>
2017-06-12 20:25:55 +0800

    Add background music.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
+//game info
 var score=0;//current score
 var life=5;//player's life left,game end when decreased to 0
 var difficulty=1;//difficult level
 var volume=50;//background music volume
 
-var container=document.getElementById('gameContainer');//get canvas
-container.style.position = 'absolute';
-container.style.top = 0;
-container.style.left = 0;
+var container=document.getElementById('gameContainer');//get game container
 
-var scene =new THREE.Scene();//create game scene
+//create game scene
+var scene =new THREE.Scene();
 
+
+//create perspective camera
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );//initialize a three.js camera
 camera.position.set( 0, 200, 200 );
 
-//create renderer
+
+//create game renderer
 var renderer=new THREE.WebGLRenderer({
-    canvas: container
+    alpha: true,
+    antialias: true
 });
 renderer.setSize(window.innerWidth,window.innerHeight);
+container.appendChild(renderer.domElement);
+renderer.shadowMapEnabled=true;//add object shadow
+
 
 //render the scene
 function render() {
@@ -26,17 +32,24 @@ function render() {
 }
 render();
 
-//init leap motion control
+//initialize leap motion control
 var leapController=new Leap.Controller();
 leapController.connect();
 
+//add environmental light
+var ambient = new THREE.AmbientLight(0xffffff);
+scene.add(ambient);
 
-
+//add background music
+var audio = document.createElement('audio');
+audio.src = 'sound/Mili - NINE POINT EIGHT.mp3';
+audio.autoplay='autoplay';
+document.body.appendChild(audio);
 
 
 function startGame()
 {
-
+    
 }
 
 function pauseGame()
@@ -48,12 +61,6 @@ function resumeGame()
 {
 
 }
-
-function setBGM()
-{
-
-}
-
 
 function deadScene()
 {

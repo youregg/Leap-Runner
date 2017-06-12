a85355ce87f49de076018152110700b686005c91
陈淇格 <cqg186495@outlook.com>
2017-06-13 00:29:59 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ var scene =new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );//initialize a three.js camera
 camera.position.set( 0, 200, 200 );
 
+//add environmental light
+var ambient = new THREE.AmbientLight(0xffffff);
+scene.add(ambient);
 
 //create game renderer
 var renderer=new THREE.WebGLRenderer({
@@ -23,7 +26,6 @@ var renderer=new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth,window.innerHeight);
 container.appendChild(renderer.domElement);
 renderer.shadowMapEnabled=true;//add object shadow
-
 
 //render the scene
 function render() {
@@ -36,20 +38,67 @@ render();
 var leapController=new Leap.Controller();
 leapController.connect();
 
-//add environmental light
-var ambient = new THREE.AmbientLight(0xffffff);
-scene.add(ambient);
-
 //add background music
 var audio = document.createElement('audio');
-audio.src = 'sound/Mili - NINE POINT EIGHT.mp3';
+audio.src = "sound/Kan R. Gao - For River - Piano (Johnny's Version).mp3";
 audio.autoplay='autoplay';
+audio.loop=true;
 document.body.appendChild(audio);
 
+//plane model
+var plane=function()
+{
+    this.mesh = new THREE.Object3D();
+    this.mesh.name = "plane";
+    var cube=new THREE.BoxGeometry(80,50,50,1,1,1);
+    this.mesh.add(cube);
+}
+
+//Obstacle model
+var obstacle=function()
+{
+    this.mesh=new THREE.Object3D();
+    this.mesh.name="obstacle";
+}
+
+//Enemy model
+var enemy=function()
+{
+    this.mesh=new THREE.Object3D();
+    this.mesh.name="enemy";
+}
+
+
+//coin model
+var coin=function()
+{
+    this.mesh=new THREE.Object3D();
+    this.mesh.name="coin";
+}
+
+//sky model
+var sky=function()
+{
+    this.mesh=new THREE.Object3D();
+    this.mesh.name="sky";
+}
+
+//sea model
+var sky=function()
+{
+    this.mesh=new THREE.Object3D();
+    this.mesh.name="sea";
+}
+
+
+function init()
+{
+
+}
 
 function startGame()
 {
-    
+
 }
 
 function pauseGame()
@@ -66,6 +115,8 @@ function deadScene()
 {
 
 }
+
+
 
 
 

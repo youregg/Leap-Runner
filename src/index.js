ab53dc8b64396cfb3b22ccdb397863c92107315c
陈淇格 <cqg186495@outlook.com>
2017-06-13 20:24:11 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+//COLORS
+var Colors = {
+    red:0xf25346,
+    white:0xd8d0d1,
+    brown:0x59332e,
+    brownDark:0x23190f,
+    pink:0xF5986E,
+    yellow:0xf4ce93,
+    blue:0x68c3c0,
+
+};
+
 //game info
 var score=0;//current score
+var scoreText=document.getElementById('score');
 var life=5;//player's life left,game end when decreased to 0
+var lifeText=document.getElementById('life');
 var difficulty=1;//difficult level
 var volume=50;//background music volume
+var dead=false;
 
 var container=document.getElementById('gameContainer');//get game container
 
@@ -13,10 +28,12 @@ var scene =new THREE.Scene();
 //create perspective camera
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );//initialize a three.js camera
 camera.position.set( 0, 200, 200 );
+scene.add(camera);
 
 //add environmental light
-var ambient = new THREE.AmbientLight(0xffffff);
-scene.add(ambient);
+var light = new THREE.AmbientLight(0xffffff);
+light.position.set(0,200,0);
+scene.add(light);
 
 //create game renderer
 var renderer=new THREE.WebGLRenderer({
@@ -38,6 +55,12 @@ render();
 var leapController=new Leap.Controller();
 leapController.connect();
 
+//leap camera control
+//var cameraControl=new THREE.LeapCameraControls();
+
+//leap object control
+//var objectControl=new THREE.LeapObjectControls();
+
 //add background music
 var audio = document.createElement('audio');
 audio.src = "sound/Kan R. Gao - For River - Piano (Johnny's Version).mp3";
@@ -45,13 +68,30 @@ audio.autoplay='autoplay';
 audio.loop=true;
 document.body.appendChild(audio);
 
-//plane model
-var plane=function()
+//player model
+var player=function()
 {
     this.mesh = new THREE.Object3D();
-    this.mesh.name = "plane";
-    var cube=new THREE.BoxGeometry(80,50,50,1,1,1);
+    this.mesh.name = "player";
+
+    var geometry=new THREE.CubeGeometry(100, 100, 100, 10, 10, 10);
+    var material=new THREE.MeshPhongMaterial({color:Colors.red});
+    var cube=new THREE.Mesh(geometry,material);
+
+    cube.castShadow=true;
+    cube.receiveShadow=true;
     this.mesh.add(cube);
+
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+}
+
+function createPlayer()
+{
+    player = new player();
+    player.mesh.scale.set(.25,.25,.25);
+    player.mesh.position.y = 100;
+    scene.add(player.mesh);
 }
 
 //Obstacle model
@@ -61,19 +101,21 @@ var obstacle=function()
     this.mesh.name="obstacle";
 }
 
-//Enemy model
-var enemy=function()
+function createObstacle()
 {
-    this.mesh=new THREE.Object3D();
-    this.mesh.name="enemy";
-}
 
+}
 
 //coin model
 var coin=function()
 {
     this.mesh=new THREE.Object3D();
     this.mesh.name="coin";
+}
+
+function createCoin()
+{
+
 }
 
 //sky model
@@ -83,15 +125,12 @@ var sky=function()
     this.mesh.name="sky";
 }
 
-//sea model
-var sky=function()
+function createSky()
 {
-    this.mesh=new THREE.Object3D();
-    this.mesh.name="sea";
+
 }
 
-
-function init()
+function crash()
 {
 
 }
@@ -111,7 +150,7 @@ function resumeGame()
 
 }
 
-function deadScene()
+function dead()
 {
 
 }
